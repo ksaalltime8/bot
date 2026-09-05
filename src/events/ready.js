9329775f21c82f3dfefe20286d95464853e6f2d2
ksaalltime8 <noureenk27@gmail.com>
2026-09-05 21:25:19 +0300

    Update ready.js

--- src/events/ready.js
+++ src/events/ready.js
@@ -6,14 +6,16 @@ module.exports = {
         console.log(`✅ Logged in as ${client.user.tag}`);
         console.log(`🌐 Serving ${client.guilds.cache.size} server(s)`);
 
-        client.user.setPresence({
-            activities: [
-                {
-                    name: "/help, Made by iik27",
-                    type: 0
-                }
-            ],
-            status: "streaming"
-        });
+     client.user.setPresence({
+    activities: [
+        {
+            name: "https://k7devs.com",
+            type: 1,
+            url: "https://k7devs.com"
+        }
+    ],
+    status: "streaming"
+});
+
     }
 };
