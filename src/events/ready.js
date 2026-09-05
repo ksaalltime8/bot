3db0abd6988a32eb1c8f32442e7ff7a64ae92128
ksaalltime8 <noureenk27@gmail.com>
2026-09-05 22:26:02 +0300

    Update ready.js

--- src/events/ready.js
+++ src/events/ready.js
@@ -6,19 +6,17 @@ module.exports = {
         console.log(`✅ Logged in as ${client.user.tag}`);
         console.log(`🌐 Serving ${client.guilds.cache.size} server(s)`);
 
-const { Client, GatewayIntentBits, ActivityType } = require('discord.js');
+client.user.setPresence({
+    activities: [
+        {
+            name: "/help, Made by iik27",
+            type: 1,
+            url: "https://k7devs.com"
+        }
+    ],
+    status: "online"
+});
 
-const client = new Client({ intents: [GatewayIntentBits.Guilds] });
-
-client.once('ready', () => {
-    client.user.setPresence({
-        activities: [{
-            name: 'iik27',
-            type: ActivityType.Streaming,
-            url: 'https://kick.com/iik27'
-        }],
-        status: 'online',
-    });
 
 
 
